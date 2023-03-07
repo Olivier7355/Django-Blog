bc86a79fe5855940c92958a02b15bab869c7785a
Olivier7355 <108932082+Olivier7355@users.noreply.github.com>
2023-03-07 09:03:01 +0000

    Last version

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -13,12 +14,13 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
+    print('Hellocccccc!')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2,
@@ -36,3 +38,8 @@ class Meta:
 
 def __str__(self):
     return self.title
+
+def get_absolute_url(self):
+    print('Hello!')
+    print(self.publish.year,self.publish.month,self.publish.day,self.slug)
+    return reverse('blog:post_detail', args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
